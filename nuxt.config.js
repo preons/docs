52034b5551dd760c3caee846e23d668fe4077f8b
Gemma Black <gblackuk@googlemail.com>
2020-05-23 15:26:10 +0100

    fix: generated routes missing

--- nuxt.config.js
+++ nuxt.config.js
@@ -103,8 +103,7 @@ module.exports = {
       const {
         $content
       } = require('@nuxt/content')
-      const files = await $content().only(['path']).fetch()
-
+      const files = await $content('articles').only(['path']).fetch()
       return files.map(file => file.path === '/index' ? '/' : file.path)
     }
   }
