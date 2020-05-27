48e252952db24ccd66e90b9a8cdabcf30767bee9
Gemma Black <gblackuk@googlemail.com>
2020-05-27 04:31:00 +0100

    fix: homepage example image

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="lh0 fs0 fs1-m lh1-m">
-    <div
-      class="pl1 pr1 pb1 pl2-m pr2-m pb2-m relative ml-au mr-au mw-xxxsuper ml8-l mt3-l"
-    >
-      <!-- Title -->
-      <h1
-        class="fs3 lh3 tu mb2 mt1 fwb mw-super maxw-xxxsuper-m fs5-m lh5-m fs6-l lh6-l"
+    <div class="lh0 fs0 fs1-m lh1-m w-100">
+      <div
+        class="pl1 pr1 pb1 pl2-m pr2-m pb2-m relative ml-au mr-au mw-xxxsuper ml4-l mt3-l relative w-100"
       >
-        A functional CSS system for building user interfaces 🛰
-      </h1>
-      <p class="mb10-m">
-        <nuxt-link to="/learn">
-          <button
-            class="bwa2 bg-transparent bca-hotpink hotpink h4 pl3 pr3 bra4"
-          >
-            Read the docs
-          </button>
-        </nuxt-link>
-      </p>
+        <h1 class="fs3 lh3 tu mb2 mt1 fwb mw-super maxw-xxsuper-m fs5-m lh5-m fs6-l lh6-l relative">
+          A functional CSS system for building user interfaces 🛰
+          <img
+            src="https://raw.githubusercontent.com/preons/preons/master/docs/notes/images/preons-tech.png"
+            class="dn db-m absolute l-90 tn10 z-1"
+          />
+        </h1>
+        <p class="mb10-m">
+          <a href="/learn" class>
+            <button class="bwa2 bg-transparent bca-hotpink hotpink h4 pl3 pr3 bra4">Read the docs</button>
+          </a>
+        </p>
+      </div>
+      <footer class="df justify-between items-center pa1 pl2-m pr2-m z-2 absolute b0 l0 w-100">
+        <a class="tdx white ml-au-m mr1 relative">
+          <span>Made withs Preons</span>
+        </a>
+      </footer>
     </div>
-    <footer
-      class="df justify-between items-center pa1 pl2-m pr2-m z-2 absolute b0 l0 w-100"
-    >
+    <footer class="df justify-between items-center pa1 pl2-m pr2-m z-2 absolute b0 l0 w-100">
       <a class="tdx white ml-au-m mr1 relative">
         <span>Made withs Preons</span>
       </a>
